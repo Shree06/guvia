beedfefcab2b4a60192188ff0b51e228cea43528
Shree06 <shree.sparkz@gmail.com>
2016-06-24 22:26:38 +0530

    Create compare_two_string.c

---
+++ compare_two_string.c
@@ -0,0 +1,41 @@
+//Comparing two strings without using strcat() function
+
+#include<stdio.h>
+
+void main()
+{
+
+    char str1[100],str2[100];
+    int i=0,a=0;
+    clrscr();
+
+
+    printf("Enter first string: ");
+    scanf("%s",str1);
+
+    printf("Enter second string: ");
+    scanf("%s",str2);
+
+    while(str1[i]&&str2[i]!='\0')
+    {
+      if(str1[i]!=str2[i])
+      a=1;
+      i++;
+    }
+    if(a==0)
+    printf("The given string are same");
+    else
+    printf("The given string is different");
+    getch();
+}
+
+
+
+
+
+
+ 
+
+
+   
+
